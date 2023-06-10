eb853e879a9026f82f361c3451697705b5064a2b
sansjtw <sansjtw@163.com>
2023-06-10 11:42:48 +0800

    Site updated: 2023-06-10 11:42:45

---
+++ js/weather.js
@@ -0,0 +1,1 @@
+document.writeln("<iframe scrolling=\'no\' src=\'https://tianqiapi.com/api.php?style=tv&skin=sogou\' frameborder=\'0\' width=\'150\' height=\'50\' allowtransparency=\'true\'></iframe>");
